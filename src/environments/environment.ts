4cb9db8e5e5f976ad18fcd17f10730408ed8fa72
jasonaiii <jason@aiii.ai>
2022-09-16 10:05:10 +0800

    調整yaml

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDYMujxQIjRxTUeE_8TMd7GuspaGzEcOLU",
+    authDomain: "fir-a7643.firebaseapp.com",
+    projectId: "fir-a7643",
+    storageBucket: "fir-a7643.appspot.com",
+    messagingSenderId: "344884773254",
+    appId: "1:344884773254:web:fba1fc9d108bec721dec07"
+  },
   production: false
 };
 
